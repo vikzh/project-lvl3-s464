867f114002957a78ebad4758256abc944b2f7cdd
vikzh <vikt.anmail@gmail.com>
2019-05-27 14:49:06 +0300

    respons added

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import isURL from 'validator/lib/isURL';
+import axios from 'axios';
 
 export default () => {
   const rssInput = document.getElementById('rss-input');
+  const button = document.getElementById('add-rss-button');
+  const corsUrl = 'https://cors.io/?';
 
   rssInput.addEventListener('input', ({ target }) => {
-    console.log(isURL(target.value));
-
     if (!isURL(target.value)) {
       rssInput.classList.add('is-invalid');
     } else {
       rssInput.classList.replace('is-invalid', 'is-valid');
     }
+  });
+
+  button.addEventListener('click', () => {
+    axios.get(corsUrl + rssInput.value)
+      .then((response) => {
+        console.log(response);
+      });
   });
 };
